af0af558bd052e964f127ab89480410d4e0a92fa
JaysenStark <838459914@qq.com>
2017-06-13 15:46:06 +0800

    part commit, add precomputeBuckets()

--- src/abstraction/CardAbstraction.java
+++ src/abstraction/CardAbstraction.java
@@ -3,10 +3,12 @@ package abstraction;
 import node.BettingNode;
 import acpc.Action;
 import acpc.Game;
+import acpc.Hand;
 
 public abstract class CardAbstraction {
 	public abstract int numBuckets(Game game, BettingNode node);
 	public abstract int numBuckets(Game game);
 	public abstract int getBucket(Game game, BettingNode node);
-	public abstract void precomputeBuckets(Game game);
+	public abstract void precomputeBuckets(Game game, Hand hand);
+	public abstract boolean canPrecomputeBuckets();
 }
--- src/abstraction/NullCardAbstraction.java
+++ src/abstraction/NullCardAbstraction.java
@@ -1,34 +1,70 @@
 package abstraction;
 
 import node.BettingNode;
+import acpc.Card;
 import acpc.Game;
+import acpc.Hand;
 
 public class NullCardAbstraction extends CardAbstraction {
 
 	public int mNumBuckets[];
+	public Game game;
+	
+	public NullCardAbstraction(Game game) {
+		this.game = game;
+		mNumBuckets = new int[game.numRounds];
+	}
 	
 	@Override
 	public int numBuckets(Game game, BettingNode node) {
-		// TODO Auto-generated method stub
-		return 0;
+		return mNumBuckets[node.getRound()];
 	}
 
 	@Override
 	public int numBuckets(Game game) {
-		// TODO Auto-generated method stub
-		return 0;
+		return mNumBuckets[game.state.round];
 	}
 
 	@Override
 	public int getBucket(Game game, BettingNode node) {
-		// TODO Auto-generated method stub
-		return 0;
+		int [] boardCards = game.state.boardCards;
+		int [] currentPlayerHoleCards = game.state.holeCards[node.getPlayer()];
+		int round = node.getRound();
+		return getBucketInternal(game, boardCards, currentPlayerHoleCards, round);
+	}
+
+	private int getBucketInternal(Game game, int[] boardCards, int[] currentPlayerHoleCards, int round) {
+		int bucket = 0;
+		int deckSize = game.numRanks * game.numSuits;
+		for ( int i = 0; i < game.numHoleCards; ++i ) {
+			if ( i > 0 ) {
+				bucket *= deckSize;
+			}
+			int card = currentPlayerHoleCards[i];
+			bucket += Card.rankOfCard(card) * game.numSuits + Card.suitOfCard(card);
+		}
+		for ( int r = 0; r <= round; ++r ) {
+			for ( int i = game.bcStart(r); i < game.sumBoardCards(r); ++i) {
+				bucket *= deckSize;
+				int card = boardCards[i];
+				bucket += Card.rankOfCard(card) * game.numSuits + Card.suitOfCard(card);
+			}
+		}
+		return bucket;
 	}
 
 	@Override
-	public void precomputeBuckets(Game game) {
-		// TODO Auto-generated method stub
+	public void precomputeBuckets(Game game, Hand hand) {
+		for ( int p = 0; p < game.numPlayers; ++p ) {
+			for (int r = 0; r < game.numRounds; ++r ) {
+				hand.precomputedBuckets[p][r] = getBucketInternal(game, hand.boardCards, hand.holeCards[p], r);
+			}
+		}
+	}
 
+	@Override
+	public boolean canPrecomputeBuckets() {
+		return true;
 	}
 
 }
