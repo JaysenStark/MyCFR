94e7876513eba825b8ed6862307da9e099bad4f1
JaysenStark <838459914@qq.com>
2017-06-14 16:53:49 +0800

    modify getBucket() in CardAbstraction.java, add 1 parameter hand

--- src/abstraction/CardAbstraction.java
+++ src/abstraction/CardAbstraction.java
@@ -8,7 +8,7 @@ import acpc.Hand;
 public abstract class CardAbstraction {
 	public abstract int numBuckets(Game game, BettingNode node);
 	public abstract int numBuckets(Game game);
-	public abstract int getBucket(Game game, BettingNode node);
+	public abstract int getBucket(Game game, BettingNode node, Hand hand);
 	public abstract void precomputeBuckets(Game game, Hand hand);
 	public abstract boolean canPrecomputeBuckets();
 }
