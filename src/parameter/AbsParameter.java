8016dd9613de803c5b0f9c8de6efe3c4f68b6928
JaysenStark <838459914@qq.com>
2017-06-15 16:19:52 +0800

    add default constructor

--- src/parameter/AbsParameter.java
+++ src/parameter/AbsParameter.java
@@ -5,9 +5,21 @@ public class AbsParameter {
 	public String gameType;
 	public String cardAbsType;
 	public String actionAbsType;
+	public boolean doAverage;
 	
 	public AbsParameter() {
-		
+		// set some default params
+		gameType = "Kuhn";
+		cardAbsType = "NullCardAbstraction";
+		actionAbsType = "NullActionAbstraction";
+		doAverage = true;
+	}
+	
+	public AbsParameter(String gameType, String cardAbsType, String actionAbsType, boolean doAverage) {
+		this.gameType = gameType;
+		this.cardAbsType = cardAbsType;
+		this.actionAbsType = actionAbsType;
+		this.doAverage = doAverage;
 	}
 	
 }
