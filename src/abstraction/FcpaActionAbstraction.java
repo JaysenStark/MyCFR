be6e95d911ff7159a84eac37fd62b4ba47f53f52
JaysenStark <838459914@qq.com>
2017-06-15 19:00:42 +0800

    add FcpaActionAbstration, currently not implemented yet

---
+++ src/abstraction/FcpaActionAbstraction.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+import acpc.Action;
+import acpc.Game;
+
+public class FcpaActionAbstraction extends ActionAbstraction {
+
+	@Override
+	public int getActions(Game game, Action[] actions) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
