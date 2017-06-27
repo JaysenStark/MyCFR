6b20adc9a10d499f691ccb9cee2223b3c9639d71
JaysenStark <838459914@qq.com>
2017-06-27 14:34:50 +0800

    add toString()

--- src/acpc/Action.java
+++ src/acpc/Action.java
@@ -7,4 +7,26 @@ public class Action {
 		this.type = type;
 		this.size = size;
 	}
+	
+	public String toString() {
+		String ret = null;
+		
+		switch (type) {
+		case a_call :
+			ret = "c";
+			break;
+		case a_fold :
+			ret = "f";
+			break;
+		case a_raise :
+			ret = "r" + size;
+			break;
+		default :
+			System.out.println("ERROR: unexpected action!");
+			System.exit(-1);
+			break;
+		}
+		
+		return ret;
+	}
 }
