9e2e141dd9b7a0c739dd272c0b2066cc81fe8446
JaysenStark <838459914@qq.com>
2017-06-27 14:37:27 +0800

    add support for string to card(int)

--- src/acpc/Card.java
+++ src/acpc/Card.java
@@ -17,4 +17,46 @@ public class Card {
 		return rank * MAX_SUITS + suit;
 	}
 
+	public static int makeCard(char rankChar, char suitChar) {
+		int rank = -1, suit = -1;
+		switch (rankChar) {
+			case 'T' :
+				rank = 8;
+				break;
+			case 'J' :
+				rank = 9;
+				break;
+			case 'Q' :
+				rank = 10;
+				break;
+			case 'K' :
+				rank = 11;
+				break;
+			case 'A' :
+				rank = 12;
+				break;
+			default:
+				rank = Character.getNumericValue(rankChar) - 2;
+		}
+
+		switch (suitChar) {
+			case 'c' :
+				suit = 0;
+				break;
+			case 'd' :
+				suit = 1;
+				break;
+			case 'h' :
+				suit = 2;
+				break;
+			case 's' :
+				suit = 3;
+				break;
+			default:
+				System.out.println("ERROR: suit must be in cdhs!");
+				System.exit(-1);
+		}
+		return rank * MAX_SUITS + suit;
+	}
+
 }
